6961a1b37fdd0d6f99ae7b80381996f847fc3fd8
plutos28 <gamevictor28@gmail.com>
2024-02-09 10:48:38 +0300

    all basic reports pages generated

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,23 @@
 class ReportsController < ApplicationController
+    def index 
+        @reports = Report.all
+    end
+
+    def new
+        @report = Report.new
+    end
+
+    def create
+      @report = Report.new(report_params)
+      if @report.save
+          redirect_to @report, notice: 'Report was successfully created.'
+      else
+          render :new
+      end
+    end
+    
+    private
+      def report_params
+        params.require(:report).permit(:title, :description, :photo, :type_of_crime)
+      end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,6 @@
 class Report < ApplicationRecord
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :type_of_crime, presence: true
 end
