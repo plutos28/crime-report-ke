b5bb59a1d71f30edadcc22614ac382c3b27807f5
plutos28 <gamevictor28@gmail.com>
2024-02-23 21:46:33 +0300

    once logged in, the ui changes to the actual app interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+        # Customize the path according to your requirements
+        new_report_path
+      end
 end
