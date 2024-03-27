3e94d6924c907afbc34e3e268ec25f9bb1475639
plutos28 <gamevictor28@gmail.com>
2024-03-28 01:21:39 +0300

    simplify admin dashboard route and hide sign up nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'admin_dashboard/index'
+  get '/admin_dashboard', to: 'admin_dashboard#index', as: 'admin_dashboard'
   devise_for :admins
   devise_for :users
   root "pages#index"
